c5e522b7cad1ce6c404d7e7cd8c06b019917a725
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-04-30 19:51:33 -0400

    add_teste_upd3

--- chat/servidor.py
+++ chat/servidor.py
@@ -8,8 +8,15 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen()
 
+salas = {}
+
 while True:
     client, addr = server.accept()
     client.send(b'Sala')
     sala = client.recv(1024).decode()
     name = client.recv(1024).decode()
+    if sala not in salas.keys():
+        salas[sala] = []
+    else:
+        sala[sala].append(client)
+        print(sala)
