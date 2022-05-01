266c58b283a385229e904895ba6298a558816fa1
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-04-30 22:19:59 -0400

    create_interface_client_upd2

--- chat/cliente.py
+++ chat/cliente.py
@@ -3,3 +3,18 @@ import threading
 from tkinter import *
 import tkinter
 from tkinter import simpledialog
+
+class Chat:
+    def __init__(self):
+        HOST = 'localhost'
+        PORT = 55556
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((HOST, PORT))
+        login = Tk()
+        login.withdraw()
+
+        self.janela_carregada = False
+        self.ativo = True
+
+        self.nome = simpledialog.askstring('Nome', 'Digite seu nome!', parent=login)
+        self.sala = simpledialog.askstring('Sala', 'Digite a sala que deseja etrar!', parent=login)
