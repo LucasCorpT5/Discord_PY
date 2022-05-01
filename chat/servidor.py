7bf8ba5eb21bc9b664dc1a2043f955afbf2f68d7
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-04-30 20:54:52 -0400

    config_server_upd4

--- chat/servidor.py
+++ chat/servidor.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 HOST = 'localhost'
-PORT = 55555
+PORT = 55556
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
@@ -12,11 +12,10 @@ salas = {}
 
 while True:
     client, addr = server.accept()
-    client.send(b'Sala')
+    client.send(b'SALA')
     sala = client.recv(1024).decode()
     name = client.recv(1024).decode()
     if sala not in salas.keys():
         salas[sala] = []
-    else:
-        sala[sala].append(client)
-        print(sala)
+    salas[sala].append(client)
+    print(salas)
--- chat/teste.py
+++ chat/teste.py
@@ -1,14 +1,12 @@
 import socket
-from time import sleep
 
 HOST = 'localhost'
-PORT = 55555
+PORT = 55556
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((HOST, PORT))
 
 mensagem = client.recv(1024)
 if mensagem == b'SALA':
-    client.send(b'Jogos')
-    sleep[1]
+    client.send(b'games')
     client.send(b'Lucas')
