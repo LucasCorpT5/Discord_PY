eb6e1d5cfba3d1111d5f9fc92ad8193a7fef32a5
LucasCorpT5 <lucasaraujotda13@gmail.com>
2022-04-30 23:06:41 -0400

    create_interface_client_upd6

--- chat/cliente.py
+++ chat/cliente.py
@@ -27,5 +27,16 @@ class Chat:
 
         self.caixa_texto = Text(self.root)
         self.caixa_texto.place(relx=0.05, rely=0.01, width=780, height=680)
+
+        self.envia_mensagem = Entry(self.root)
+        self.envia_mensagem.place(relx=0.05, rely=0.8, width=500, height=20)
+
+        self.btn_enviar = Button(self.root, text='Enviar', command=self.enviarMensagem)
+        self.btn_enviar.place(relx=0.05, rely=0.8, width=100, height=20)
+
         self.root.mainloop()
+
+    def enviarMensagem(self):
+        pass
+
 chat = Chat()
